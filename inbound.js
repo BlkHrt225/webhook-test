690692a97d41020e03787e49501ba1aa9dca36bc
C:/Program Files/Git/Blkhrt225 <rahulsingh793@gmail.com>
2021-07-06 13:56:53 +0530

    ok

--- inbound.js
+++ inbound.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express();
+const DataStore = require('nedb')
+const InDb = new DataStore({filename:'./db/Incoming',autoload:true})
+const OutDb = new DataStore({filename:'./db/OutGoing',autoload:true})
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT||3000
@@ -8,8 +11,14 @@ app.get("/",(req,res)=>{
     
 })
 
-
+app.get("/In",(req,res)=>{
+    
+})
 app.get("/incoming",(req,res)=>{
-    console.log(req.query)
+    var record = req.query
+    InDb.insert(record,(err,doc)=>{
+        if(err) console.log(err)
+        if(doc) console.log(doc)
+    })
 })
 app.listen(port,()=>console.log('Running'))
