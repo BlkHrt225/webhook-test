81b6a6a40c6fdd0461bfb4403674536cd941dc61
C:/Program Files/Git/Blkhrt225 <rahulsingh793@gmail.com>
2021-07-06 13:21:42 +0530

    ok

--- inbound.js
+++ inbound.js
@@ -4,9 +4,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT||3000
 app.get("/",(req,res)=>{
-    var val = req.params
-    console.log(val)
+   res.send("Hello")
     
 })
 
+
+app.get("/incoming",(req,res)=>{
+    console.log(req.params)
+})
 app.listen(port,()=>console.log('Running'))
