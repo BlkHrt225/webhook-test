086fc41d678c60da7b3708081d8706d9ac6188ec
C:/Program Files/Git/Blkhrt225 <rahulsingh793@gmail.com>
2021-07-06 14:18:43 +0530

    ok commit

--- inbound.js
+++ inbound.js
@@ -11,14 +11,36 @@ app.get("/",(req,res)=>{
     
 })
 
-app.get("/In",(req,res)=>{
-    
+app.get("/getIn",(req,res)=>{
+    InDb.find({status:'ANSWERED'},(err,doc)=>{
+        if(err)res.status(500)
+        else(res.send(doc))
+    })
+})
+app.get('/getMis',(req,res)=>{
+    InDb.find({status:'NOANSWER'},(err,doc)=>{
+        if(err)res.status(500)
+        if(doc)res.send(doc)
+    })
+})
+app.post('/postOut',(req,res)=>{
+    const record= {Caller,Callee,Time}= req.body
+    OutDb.insert(record,(err,doc)=>{
+        if(err) console.log(err)
+        if(doc) console.log(doc)
+    })
 })
 app.get("/incoming",(req,res)=>{
     var record = req.query
     InDb.insert(record,(err,doc)=>{
         if(err) console.log(err)
         if(doc) console.log(doc)
+    })
+})
+app.get('/getOut',(req,res)=>{
+    InDb.find({},(err,doc)=>{
+        if(err)res.status(500)
+        if(doc)res.send(doc)
     })
 })
 app.listen(port,()=>console.log('Running'))
